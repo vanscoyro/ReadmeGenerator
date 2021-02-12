ab913b57606dd635f0721f627b2e98ea630f75cf
Rob V <rvans003@fiu.edu>
2021-02-12 11:33:16 -0500

    added prompt questions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,8 @@
 // array of questions for user
 const questions = [
-
+    "What is your project's title?", "Please give a brief description of your project", "What are the installation instructions?",
+    "What are your contribution guidelines?", "Do you want to add a section for Test cases?", "What license would you like for your repo?",
+    "What is your GitHub username?", "What is your email address?"
 ];
 
 // function to write README file
