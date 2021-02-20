25dcb538a4087820f4daccd1d5ac7c6d2b90123f
Rob V <rvans003@fiu.edu>
2021-02-20 14:31:20 -0500

    reworked question object to fit inquirer requirements

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,72 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
 // array of questions for user
 const questions = [
-    "What is your project's title?", "Please give a brief description of your project", "What are the installation instructions?",
-    "What are your contribution guidelines?", "Do you want to add a section for Test cases?", "What license would you like for your repo?",
-    "What is your GitHub username?", "What is your email address?"
+    {
+        type: "input",
+        message: "What is your name?",
+        name : "name",
+    },
+    {
+        type: "input",
+        message: "What is your Github Username?",
+        name : "gitHubUserName",
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name : "contact",
+    },
+    {
+        type: "input",
+        message: "What is your project's title?",
+        name : "title",
+    },
+    {
+        type: "input",
+        message: "Please give a brief description of your project",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "directions",
+    },
+    {
+        type: "input",
+        message:"What are your contribution guidelines?",
+        name: "contributions",
+    },
+    {
+        type: "input",
+        message:"What kind of license should your project have?",
+        choices : [
+            "Apache",
+            "MIT",
+            "ISC",
+            "GNU GPLv3",
+        ],
+        name: "license",
+    },
+
+    {
+        type: "list",
+        message:"Would you like to add a section for Test cases?",
+        choices : [
+            "Yes",
+            "No,"
+        ],
+        name: "testCaseQuestion",
+    },
+
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
