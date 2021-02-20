430eed6735812a1cdee0a846e015fa817d56370f
Rob V <rvans003@fiu.edu>
2021-02-20 16:46:38 -0500

    added validation to all input fields

--- Develop/index.js
+++ Develop/index.js
@@ -9,37 +9,50 @@ const questions = [
         type: "input",
         message: "What is your project's title?",
         name : "title",
+        validate: (value)=> { if (value){return true} else {return "Please enter project title"}},
     },
     {
         type: "input",
         message: "What is your name?",
         name : "name",
+        validate: (value)=> { if (value){return true} else {return "Please enter your name"}},
+
     },
     {
         type: "input",
         message: "What is your Github Username?",
         name : "gitHubUserName",
+        validate: (value)=> { if (value){return true} else {return "Please enter your GitHub username"}},
+
     },
     {
         type: "input",
         message: "What is your email?",
         name : "contact",
+        validate: (value)=> { if (value){return true} else {return "Please enter your email"}},
+
     },
 
     {
         type: "input",
         message: "Please give a brief description of your project",
         name: "description",
+        validate: (value)=> { if (value){return true} else {return "Please enter a brief description of your project"}},
+
     },
     {
         type: "input",
         message: "What are the installation instructions?",
         name: "directions",
+        validate: (value)=> { if (value){return true} else {return "Please enter the installation instructions for your project"}},
+
     },
     {
         type: "input",
         message:"What are your contribution guidelines?",
         name: "contributions",
+        validate: (value)=> { if (value){return true} else {return "Please the contribution guidelines for your project"}},
+
     },
     {
         type: "list",
@@ -51,6 +64,8 @@ const questions = [
             "GNU GPLv3",
         ],
         name: "license",
+        validate: (value)=> { if (value){return true} else {return "Please choose one of the licenses"}},
+
     },
     {
         type: "list",
@@ -62,11 +77,15 @@ const questions = [
             "node.js",
         ],
         name: "language",
+        validate: (value)=> { if (value){return true} else {return "Please choose a main language"}},
+
     },
     {
         type: "input",
         message:"Please provide information for any Test Cases",
         name: "testCase",
+        validate: (value)=> { if (value){return true} else {return "Please enter any information about how to preform a test on your project"}},
+
     },
 
 ];
